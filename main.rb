95c4b8a236d6370d8cbb4f1e873f0b2908bc8610
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-04-12 15:39:52 -0400

    Initialized base case and split functionality

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
 # Frozen_string_literal: true
+
+def merge_sort(array)
+  return array if array.length <= 1
+
+  left_half = array.slice(0, (array.length / 2))
+  right_half = array.slice((array.length / 2), (array.length - 1))
+end
+
+my_array = [1, 2, 3, 4, 5, 6, 7]
+p merge_sort(my_array)
