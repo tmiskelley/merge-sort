020cf0cc3fd0704aeb400ab2fb0bc2462db6000a
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-05-08 19:00:06 -0400

    Added recursive function call

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
 # Frozen_string_literal: true
 
+require 'pry'
+
 def merge_sort(array)
   return array if array.length <= 1
 
   left_half = array.slice(0, (array.length / 2))
   right_half = array.slice((array.length / 2), (array.length - 1))
+
+  merge_sort(left_half)
+  merge_sort(right_half)
 end
 
-my_array = [1, 2, 3, 4, 5, 6, 7]
+my_array = [7, 4, 5, 6, 3, 1, 2]
 p merge_sort(my_array)
